214b8e8147102efeb9b9b271c1034043823ff6ca
Dimo Petrov <dpetrov@devexperts.com>
2019-04-20 10:11:44 +0300

    rename some test to reflect the current service interface names

--- src/test/java/where/to/refuel/server/model/service/PetrolStationsServiceTest.java
+++ src/test/java/where/to/refuel/server/model/service/PetrolStationsServiceTest.java
@@ -4,25 +4,34 @@ import io.micronaut.test.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import where.to.refuel.server.model.Coordinates;
 import where.to.refuel.server.model.FuelType;
+import where.to.refuel.server.model.PetrolStation;
 
 import javax.inject.Inject;
-import java.util.Map;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautTest
-class FuelPriceClientTest extends IntegrationTest {
+class PetrolStationsServiceTest extends IntegrationTest {
 
   private static final Coordinates VALID_COORDINATES = Coordinates.of(42.6567825, 23.2857181);
+  private static final Coordinates INVALID_COORDINATES = Coordinates.of(0.12, 0.213);
 
   @Inject
-  private PetrolStationPriceService service;
+  private PetrolStationsService service;
 
   @Test
   void findByLocationShouldReturnValidResults() {
-    Map<Integer, Double> result = service.findByLocationAndFuelType(VALID_COORDINATES, FuelType.LPG);
+    List<PetrolStation> result = service.findByLocationAndFuelType(VALID_COORDINATES, FuelType.LPG);
     assertNotNull(result);
     assertEquals(50, result.size());
+  }
+
+  @Test
+  void findByLocationShouldReturnEmptyListIfNoPetrolStationsAreFound() {
+    List<PetrolStation> result = service.findByLocationAndFuelType(INVALID_COORDINATES, FuelType.LPG);
+    assertNotNull(result);
+    assertEquals(0, result.size());
   }
 }

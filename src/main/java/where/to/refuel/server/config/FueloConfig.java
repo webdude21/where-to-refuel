957a445ec07279d08fd1e994eef18759762a6419
Dimo Petrov <dpetrov@devexperts.com>
2019-04-20 09:37:39 +0300

    fix build

--- src/main/java/where/to/refuel/server/config/FueloConfig.java
+++ src/main/java/where/to/refuel/server/config/FueloConfig.java
@@ -3,13 +3,12 @@ package where.to.refuel.server.config;
 import io.micronaut.context.annotation.ConfigurationProperties;
 import io.micronaut.context.annotation.Requires;
 import lombok.Data;
-import lombok.Getter;
 
 @ConfigurationProperties(FueloConfig.PREFIX)
 @Data
 @Requires(property = FueloConfig.PREFIX)
 @Requires(property = FueloConfig.PREFIX + ".apikey")
-public class FueloConfig {
+class FueloConfig {
   static final String PREFIX = "fuelo";
   private String apikey;
 }

73b6198542f638c487701d12413609dfff63b8c6
Dimo Petrov <dpetrov@devexperts.com>
2019-04-20 10:09:31 +0300

    add a test for the FuelPriceService

--- src/test/java/where/to/refuel/server/model/service/FuelPriceClientTest.java
+++ src/test/java/where/to/refuel/server/model/service/FuelPriceClientTest.java
@@ -4,34 +4,25 @@ import io.micronaut.test.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import where.to.refuel.server.model.Coordinates;
 import where.to.refuel.server.model.FuelType;
-import where.to.refuel.server.model.PetrolStation;
 
 import javax.inject.Inject;
-import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautTest
-class FuelServiceTest extends IntegrationTest {
+class FuelPriceClientTest extends IntegrationTest {
 
   private static final Coordinates VALID_COORDINATES = Coordinates.of(42.6567825, 23.2857181);
-  private static final Coordinates INVALID_COORDINATES = Coordinates.of(0.12, 0.213);
 
   @Inject
-  private PetrolStationsService service;
+  private PetrolStationPriceService service;
 
   @Test
   void findByLocationShouldReturnValidResults() {
-    List<PetrolStation> result = service.findByLocationAndFuelType(VALID_COORDINATES, FuelType.LPG);
+    Map<Integer, Double> result = service.findByLocationAndFuelType(VALID_COORDINATES, FuelType.LPG);
     assertNotNull(result);
     assertEquals(50, result.size());
-  }
-
-  @Test
-  void findByLocationShouldReturnEmptyListIfNoPetrolStationsAreFound() {
-    List<PetrolStation> result = service.findByLocationAndFuelType(INVALID_COORDINATES, FuelType.LPG);
-    assertNotNull(result);
-    assertEquals(0, result.size());
   }
 }
---
+++ src/test/java/where/to/refuel/server/model/service/IntegrationTest.java
@@ -0,0 +1,16 @@
+package where.to.refuel.server.model.service;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
+import org.junit.jupiter.api.BeforeAll;
+
+public class IntegrationTest {
+
+  @Rule
+  public static WireMockRule wireMock = new WireMockRule(8090);
+
+  @BeforeAll
+  static void setup() {
+    wireMock.start();
+  }
+}

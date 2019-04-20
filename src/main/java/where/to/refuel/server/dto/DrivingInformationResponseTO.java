4bbe76bf7c25b863a0801bd1e341f82eb99cbf65
Dimo Petrov <dpetrov@devexperts.com>
2019-04-20 09:42:11 +0300

    suppress unchecked cast warning

--- src/main/java/where/to/refuel/server/dto/DrivingInformationResponseTO.java
+++ src/main/java/where/to/refuel/server/dto/DrivingInformationResponseTO.java
@@ -17,6 +17,7 @@ public class DrivingInformationResponseTO {
   private List<DrivingInformationTO> drivingInformationTOs;
 
   @JsonProperty("resourceSets")
+  @SuppressWarnings("unchecked")
   private void unpackResult(List<Map<String, List>> objects) {
     var resources = objects.get(0).get("resources").get(0);
     ArrayList<LinkedHashMap<String, ? extends Number>> results = (ArrayList<LinkedHashMap<String, ? extends Number>>) ((LinkedHashMap) resources).get("results");

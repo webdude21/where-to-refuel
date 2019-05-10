f65b3281a7665db8e74dd5be8e93957e6b852466
Dimo Petrov <dpetrov@devexperts.com>
2019-05-10 21:11:12 +0300

    add logging in the http utils

--- where-to-refuel-server/src/main/java/where/to/refuel/server/util/HttpUtils.java
+++ where-to-refuel-server/src/main/java/where/to/refuel/server/util/HttpUtils.java
@@ -2,17 +2,25 @@ package where.to.refuel.server.util;
 
 import io.micronaut.http.HttpRequest;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpUtils {
+  private static final Logger log = LoggerFactory.getLogger(HttpUtils.class);
+
   public static String extractIpAddress(final HttpRequest<?> httpRequest) {
     var forwardedFor = httpRequest.getHeaders().get("HTTP_X_FORWARDED_FOR");
 
     if (StringUtils.isBlank(forwardedFor)) {
+      log.debug("Got the ip {} from the request.", forwardedFor);
       return httpRequest.getRemoteAddress().getAddress().getHostAddress();
     }
 
     var ipAddresses = StringUtils.splitPreserveAllTokens(forwardedFor, ",");
+    var lastIpAddress = ipAddresses[ipAddresses.length - 1];
 
-    return ipAddresses[ipAddresses.length - 1];
+    log.debug("Got the ip {} from the headers, full string is {}", lastIpAddress, ipAddresses);
+
+    return lastIpAddress;
   }
 }

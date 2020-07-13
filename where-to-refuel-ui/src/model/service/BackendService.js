582ae2f4bfdeb7eb3e0c10c26c79121d44c33a94
webdude21 <dimo.petrof@gmail.com>
2020-07-13 10:46:15 +0300

    inline variables

--- where-to-refuel-ui/src/model/service/BackendService.js
+++ where-to-refuel-ui/src/model/service/BackendService.js
@@ -1,9 +1,7 @@
 export async function getNearestPetrolStations({ coords: { latitude, longitude } }, fuelType) {
-  const responsePromise = await fetch(`near-by-petrol-stations?latitude=${latitude}&longitude=${longitude}&fuel=${fuelType}`);
-  return responsePromise.json();
+  return (await fetch(`near-by-petrol-stations?latitude=${latitude}&longitude=${longitude}&fuel=${fuelType}`)).json();
 }
 
 export async function getUserLogs() {
-  const resp = await fetch("analytics/user-logs");
-  return resp.json();
+  return (await fetch("analytics/user-logs")).json();
 }
